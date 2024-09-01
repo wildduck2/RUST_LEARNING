720b6d6265c3bfcce9b6243619532621632f5ca3
wild_duck <wezonaser50@gmail.com>
2024-09-01 14:09:10 +0300

    feat: bat man

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// src/main.rs
+
+// Import the `utils` module
+mod utils;
+
+fn main() {
+    // Use the `greet` function from the `utils` module
+    utils::greet("sdfasdf");
+    //sadfasdf
+    utils::greet("Rustacean");
+}
