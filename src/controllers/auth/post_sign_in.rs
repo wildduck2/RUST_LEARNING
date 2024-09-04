489d4ae735a243ad75a0d7328124dd4595fd7ada
wild_duck <wezonaser50@gmail.com>
2024-09-05 01:02:34 +0300

    chore: attepting to solve some problems

--- src/controllers/auth/post_sign_in.rs
+++ src/controllers/auth/post_sign_in.rs
@@ -1,5 +1,5 @@
 use actix_web::{HttpResponse, Responder};
 
 pub async fn post_sign_in() -> impl Responder {
-    HttpResponse::Ok().body("You're signed in. You can do whatever you want.")
+    HttpResponse::Ok().body("btw i use rust i feel, oyyhahh daddy i am so fast ->> bomb")
 }
--- src/routes/auth/sign_in.rs
+++ src/routes/auth/sign_in.rs
@@ -4,7 +4,7 @@ use crate::controllers::auth::post_sign_in::post_sign_in;
 
 pub fn config(cfg: &mut web::ServiceConfig) -> () {
     cfg.service(
-        web::resource("/app")
+        web::resource("/v1/signin")
             .route(web::get().to(post_sign_in))
             .route(web::head().to(HttpResponse::MethodNotAllowed)),
     );
